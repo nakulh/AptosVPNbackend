c9f862b1ce048573b5485345dc22a64ed2ffe486
Nakul Havelia <nakulhavelia@gmail.com>
2024-09-11 20:08:13 +0530

    init boot

--- miner/index.js
+++ miner/index.js
@@ -8,4 +8,5 @@ import { updateIPAddressIfRequired } from './helper/ipUpdate.js';
 import { addVPNClient, checkExpiredVPNConfig } from './helper/wireguardConfig.js';
 //tx for vpnprovider creation 0xec4a70e2883c95d3339a0a509c5d9ca1cc61084ec311d221d1e75864708101d1
 //provideAccess("0xaa4889db72049a54e31c65df276e717a8961e3b1486f917e15a60f01ecd87278");
-await checkExpiredVPNConfig();
+//await checkExpiredVPNConfig();
+await initBoot();
